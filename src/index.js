92b7eb7bbee726c0b55bd6b1da836742af1ba477
Guru Mahendran <me@gurubavan.com>
2020-07-15 14:34:46 -0400

    clearablility: clear on select
    also add "className", "selectStyle", "hasClearButton" props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // @flow
-import * as React from 'react';
-import { Select, Spin, Button } from 'antd';
-import debounce from 'lodash.debounce';
+import * as React from "react";
+import { Select, Spin, Button } from "antd";
+import debounce from "lodash.debounce";
 const Option = Select.Option;
 
 type Props = {
@@ -12,30 +12,33 @@ type Props = {
   radius?: number,
   types?: Array<string>,
   defaultValue?: Array<ResultObj>,
+  className?: string,
+  selectStyle?: any,
+  hasClearButton?: boolean,
   minLength: number,
   multiple: boolean,
-  onChange: (Array<ResultObj> => void)
-}
+  onChange: (Array<ResultObj>) => void,
+};
 
 type State = {
   fetching: boolean,
   value: Array<ResultObj>,
   data: Array<ResultObj>,
-  disabled: boolean
-}
+  disabled: boolean,
+};
 
 type Options = Array<{
   key: string,
-  label: string
-}>
+  label: string,
+}>;
 
 type ResultObj = {
   placeId: string,
   address: string,
   gmaps?: any,
   lat?: number,
-  lng?: number
-}
+  lng?: number,
+};
 
 export default class AntdGeosuggest extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -56,30 +59,36 @@ export default class AntdGeosuggest extends React.Component<Props, State> {
     data: [],
     value: [],
     fetching: false,
-    disabled: false
-  }
+    disabled: false,
+  };
 
   static defaultProps = {
     placeholder: "Type and search for places",
     minLength: 3,
-    multiple: false
-  }
+    multiple: false,
+    className: "antd-geosuggest",
+    selectStyle: {},
+    hasClearButton: false,
+  };
 
   componentDidMount() {
-    const {defaultValue} = this.props;
+    const { defaultValue } = this.props;
 
     if (defaultValue && defaultValue.length > 0) {
-      this.setState({value: defaultValue})
+      this.setState({ value: defaultValue });
     }
   }
 
   componentDidUpdate() {
-    const {multiple} = this.props;
-    const {value, disabled} = this.state;
+    const { multiple, hasClearButton } = this.props;
+    const { value, disabled } = this.state;
 
     if (!disabled && !multiple && value.length >= 1 && this.selection) {
-      this.selection.blur()
-      this.setState({disabled: true})
+      this.selection.blur();
+
+      if (hasClearButton) {
+        this.setState({ disabled: true });
+      }
     }
   }
 
@@ -91,44 +100,43 @@ export default class AntdGeosuggest extends React.Component<Props, State> {
     }
 
     let options = {
-      input: value
-    }
+      input: value,
+    };
 
-    const mapOptions = ['location', 'radius', 'bounds', 'types'];
-    mapOptions.forEach(option => {
+    const mapOptions = ["location", "radius", "bounds", "types"];
+    mapOptions.forEach((option) => {
       if (this.props[option]) {
         // $FlowFixMe
         options[option] = this.props[option];
       }
     });
 
-    this.setState({data: [], fetching: true}, () => {
+    this.setState({ data: [], fetching: true }, () => {
       // Get suggested locations from google map.
       this.autocompleteService.getPlacePredictions(
         options,
-        suggestsGoogle => {
+        (suggestsGoogle) => {
           const suggestions = suggestsGoogle || [];
-          const data = suggestions.map(datum => {
+          const data = suggestions.map((datum) => {
             return {
               address: datum.description,
-              placeId: datum.place_id
-            }
-          })
+              placeId: datum.place_id,
+            };
+          });
 
-          this.setState({data, fetching: false});
+          this.setState({ data, fetching: false });
         }
       );
     });
-  }
-  
+  };
+
   handleChange = (value: Options) => {
     const that = this;
     // Geocode the location using Google geocode API. In order to get location's latitude and longtitude.
     function promiseGeocode(singleSite) {
       return new Promise((resolve) => {
-
         that.geocoder.geocode(
-          {placeId: singleSite.placeId},
+          { placeId: singleSite.placeId },
           (results, status) => {
             // $FlowFixMe
             const newData: ResultObj = singleSite;
@@ -136,7 +144,7 @@ export default class AntdGeosuggest extends React.Component<Props, State> {
             if (status === google.maps.GeocoderStatus.OK) {
               const gmaps = results[0],
                 location = gmaps.geometry.location;
-    
+
               newData.gmaps = gmaps;
               newData.lat = location.lat();
               newData.lng = location.lng();
@@ -144,52 +152,83 @@ export default class AntdGeosuggest extends React.Component<Props, State> {
             resolve(newData);
           }
         );
-      })
+      });
     }
 
     // Wait for all locations to finish geocoding, and call onChange function in the end.
-    Promise.all(value.map(site => promiseGeocode({placeId: site.key, address: site.label}))).then(result => {
+    Promise.all(
+      value.map((site) =>
+        promiseGeocode({ placeId: site.key, address: site.label })
+      )
+    ).then((result) => {
       that.props.onChange(result);
-    })
-    
+    });
+
     this.setState({
       value: ((value: any): Array<ResultObj>),
       data: [],
       fetching: false,
     });
-  }
+  };
 
   clearValue = () => {
     // reset value
-    this.setState({
-      data: [],
-      value: [],
-      fetching: false,
-      disabled: false
-    }, this.props.onChange([]))
-  }
+    this.setState(
+      {
+        data: [],
+        value: [],
+        fetching: false,
+        disabled: false,
+      },
+      this.props.onChange([])
+    );
+  };
 
   render() {
     const { fetching, data, value, disabled } = this.state;
-    const { placeholder } = this.props;
+    const { className, placeholder, selectStyle, hasClearButton } = this.props;
+
+    const defaultSelectStyle = { width: "100%" };
+
+    if (hasClearButton) {
+      defaultSelectStyle.width = "80%";
+      defaultSelectStyle.marginRight = "10px";
+    }
+
     return (
-      <div>
+      <div
+        style={{ display: "flex" }}
+        className={className}
+        onClick={() => {
+          if (value && value.length) {
+            this.clearValue();
+          }
+        }}
+      >
         <Select
-          ref={node => this.selection = node}
-          mode="multiple"
+          ref={(node) => (this.selection = node)}
           labelInValue
-          value={value.map((v, i) => ({key: v.address || i, label: v.address, ...v}))}
+          mode="multiple"
+          value={value.map((v, i) => ({
+            key: v.address || i,
+            label: v.address,
+            ...v,
+          }))}
           disabled={disabled}
           placeholder={placeholder}
-          notFoundContent={!disabled && (fetching ? <Spin size="small" /> : "No result")}
+          notFoundContent={
+            !disabled && (fetching ? <Spin size="small" /> : "No result")
+          }
           filterOption={false}
           onSearch={this.fetchLocation}
           onChange={this.handleChange}
-          style={{width: "80%", marginRight: '10px'}}
+          style={{ ...defaultSelectStyle, ...selectStyle }}
         >
-          {data.map(d => <Option key={d.placeId}>{d.address}</Option>)}
+          {data.map((d) => (
+            <Option key={d.placeId}>{d.address}</Option>
+          ))}
         </Select>
-        <Button onClick={this.clearValue}>Clear</Button>
+        {hasClearButton && <Button onClick={this.clearValue}>Clear</Button>}
       </div>
     );
   }
